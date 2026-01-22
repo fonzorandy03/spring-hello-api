41b8e49d909b16898431e1ecbaa1709caec4812f
Alfonso Maria Ferrara <alfonso.mferrara@gmail.com>
2026-01-22 12:34:00 +0100

    Add Swagger OpenAPI and migrate config to application.yml

--- src/main/java/com/example/helloapi/HelloController.java
+++ src/main/java/com/example/helloapi/HelloController.java
@@ -1,13 +1,20 @@
 package com.example.helloapi;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
 
+@Tag(name = "Hello", description = "Demo endpoints")
 @RestController
 public class HelloController {
 
+    @Operation(
+            summary = "Returns a hello message",
+            description = "Simple demo endpoint returning 'Hello World' in JSON"
+    )
     @GetMapping("/hello")
     public Map<String, String> hello() {
         return Map.of("message", "Hello World");
